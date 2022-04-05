5d23508f333a3777b0b3af2f30074252ae253878
Wilson Wu <Wilson Wu@DESKTOP-PTUGGNT>
2022-04-05 15:47:46 +0800

    PUSH_TEST

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  // publicPath 為 project-name，根目錄地址為上傳的網域
+  publicPath: "/BillComparison/",
+};
